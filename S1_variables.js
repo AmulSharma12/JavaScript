518f5a0fb4d0eee0084a41d480b3493c573e4911
Amul Op <amulsharmarr67@gmail.com>
2021-12-04 16:58:53 +0530

    js variables

---
+++ S1_variables.js
@@ -0,0 +1,31 @@
+// ------------------------How to print in javascript
+console.log("Amul Sharma");
+
+
+// variable is  a container which is used to store the values 
+// var age = 20;
+// console.log(age);
+// var myName = "Amul Sharma";
+// console.log(myName);
+// var isHuman = true;
+// console.log(isHuman);
+// var nhiPata;
+// console.log(nhiPata);
+
+// ------------------------------Using Right variable naming convention
+// 1.) not start with digit 
+// 2.) no special character are allowed other than _ , $ 
+// 3.) no keywords are allowed
+// 4.) use camel case for good practice 
+// 5.) use variable of any length and meaningful 
+
+// var myName = "Amul Sharma";
+// console.log(myName);
+// var isHuman = 20;
+// console.log(isHuman);
+// var 9not_allowed;      //error
+// console.log(9not_allowed);
+// var _valid;
+// console.log(_valid);
+// var $valid;
+// console.log($valid);
