3a8844621b9683291f35a6f27faa4769a101ec86
Amul Op <amulsharmarr67@gmail.com>
2021-12-04 18:02:09 +0530

    Interview-1

---
+++ Interview-1.js
@@ -0,0 +1,33 @@
+//------------------------------------------------------how + works
+// console.log(10 + " Amul");          //data + string = concat
+// console.log("Amul " + 10);          //string + data = concat
+// console.log("Amul " + " Sharma")     //string + string = concat
+
+
+//---------------------------------------------------------how - works
+// console.log(10 - " 4 ");             //data - string  = bug
+// console.log("4" - 10);               //string(ifValue) - data(bug)
+// console.log(10 - " Sharma");        //Nan
+// console.log("Amul " - 5);           //NaN
+// console.log("Amul " - "Sharma");    //NaN
+
+
+
+// -------------------------------------------(Interview question 1)null vs undefined
+// null vs undefined
+// null ->  variable value is null  
+//undefined -> when variable declared and not initialised then value is replace with placeholder 
+// undefined 
+
+// var num = null;
+// console.log(num);
+// console.log(typeof(num));
+// var num2;
+// console.log(num2);
+// console.log(typeof(num2));
+
+
+// -----------------------------------------------------(Interview question 2)NaN
+var isnotnumber = isNaN(Number.NaN);
+console.log(isnotnumber);
+ 
