299798ce1511ca091c50e0205266358ef769a0bb
Amul Op <amulsharmarr67@gmail.com>
2021-12-04 17:06:58 +0530

    S2_Datatypes.js

---
+++ S2_Datatypes.js
@@ -0,0 +1,28 @@
+// datatypes are  the type of data/values are stored
+// normally we have the 6 type of datatypes
+
+// 1.) number
+// 2.) string
+// 3.) undefined
+// 4.) boolean
+// 5.) bigInt(ECMASCRIPT 2020)
+// 6.) symbol
+
+// -----------------------------------Demo
+// var number = 4;
+// console.log(number);
+// console.log(typeof(number));
+
+
+// var isAge = true;
+// console.log(isAge);
+// console.log(typeof(isAge));
+
+// var myName = "Amul Sharma";
+// console.log(myName);
+// console.log(typeof(myName));
+
+// var undef;
+// console.log(undef);
+// console.log(typeof(undef));
+// Later discussed
