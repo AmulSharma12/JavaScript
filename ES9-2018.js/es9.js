3307f2a321598330a9172b15c0a46d5898e2b900
Amul Op <amulsharmarr67@gmail.com>
2021-12-18 18:20:38 +0530

    ES9

---
+++ ES9-2018.js/es9.js
@@ -0,0 +1,7 @@
+// ecmascript 9/ 2018 allow the feature or you can say extend the feature of spread/rest operators
+// that is we can apply spread/rest operator in object also
+
+// const person = {name:"Amul Sharma",  age:20}
+// console.log(person)
+// const newArr = {...person}
+// console.log(newArr)
