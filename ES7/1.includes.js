ef83a893a72f592395e7ba0adb5eb98335c51318
Amul Op <amulsharmarr67@gmail.com>
2021-12-18 17:42:35 +0530

    ES7 Concept

---
+++ ES7/1.includes.js
@@ -0,0 +1,13 @@
+// ES7 provide us the includes method
+// Array.prototype.includes()
+
+
+// returns the boolean if element present in array or not 
+// if element present in array returns true
+// if element not present in array returns false
+
+const arr = ['red', 'blue', 'black', 'white', 'green']
+const isPresent = arr.includes('red')
+console.log(isPresent)
+const isNotPresent = arr.includes('python')
+console.log(isNotPresent)
---
+++ ES7/2.exponentiation.js
@@ -0,0 +1,16 @@
+// Es7 feature provide us to calculate the power to base to exponent in terms of exponentiation operator
+
+// returns the element of base to exponenet
+var num = 2;
+var  pow = 5;
+console.log(num ** pow)
+
+
+var  num = 2
+var  pow = 0 
+console.log(num ** pow)
+
+
+var num = 2
+var pow = -1
+console.log(num ** pow)
